6a6eb06949aaa9beb5f0114b129cfe23ca081cf1
Marnie Robinson <marnielane@gmail.com>
2021-01-11 22:43:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,4 +15,11 @@ def loop_message_n_times(message, limit)
     puts message
     count += 1 
   end
+  count = 0 
+  message = "Hello Red Balloon."
+  limit = 10 
+  while count < limit do
+    puts message
+    count += 1 
+  end
 end
